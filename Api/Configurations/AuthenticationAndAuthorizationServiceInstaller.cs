5974933c200c38899e8b19e6395e68be261c2464
Kamel Danial <Kamel.Danial@keitmea.com>
2023-09-02 15:47:02 +0300

    Added JWT validate

--- Api/Configurations/AuthenticationAndAuthorizationServiceInstaller.cs
+++ Api/Configurations/AuthenticationAndAuthorizationServiceInstaller.cs
@@ -2,6 +2,8 @@
 using Infrastructure.Authentications;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace Api.Configurations;
 
@@ -9,20 +11,22 @@ public class AuthenticationAndAuthorizationServiceInstaller : IServiceInstaller
 {
     public void Install(IServiceCollection services, IConfiguration configuration)
     {
+        services.ConfigureOptions<JwtOptionsSetup>();
+        services.ConfigureOptions<JwtBearerOptionsSetup>();
+
         services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             .AddJwtBearer(options => options.TokenValidationParameters = new()
             {
                 ValidateIssuer = true,
-                ValidIssuer = "",
-
+                ValidIssuer = configuration["JwtOptions:Issuer"],
+                ValidateAudience = true,
+                ValidAudience = configuration["JwtOptions:Audience"],
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtOptions:SecertKey"]!)),
             });
-
-        services.ConfigureOptions<JwtOptionsSetup>();
-        services.ConfigureOptions<JwtBearerOptionsSetup>();
 
         services.AddAuthorization();
         //services.AddSingleton<IAuthorizationHandler, PermissionAuthorizationHandler>();
         //services.AddSingleton<IAuthorizationPolicyProvider, PermissionAuthorizationPolicyProvider>();
-        services.AddScoped<IPermissionService, PermissionService>();
     }
 }
